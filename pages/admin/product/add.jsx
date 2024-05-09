0e638fd5ee5c8a812ce3b09f3be2b7826e7673a1
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-05-09 17:38:23 +0300

    add

--- pages/admin/product/add.jsx
+++ pages/admin/product/add.jsx
@@ -1,34 +1,66 @@
-import React, { useState } from 'react';
-import { Input,Box,Button ,Stack} from '@chakra-ui/react';
-import AddProductMain from '@/components/admin/product/addProduct';
-import { getDocuments } from '@/functions/firebase/getData';
+import React, { useState, useEffect } from "react";
+import { Input, Box, Button, Stack } from "@chakra-ui/react";
+import AddProductMain from "@/components/admin/product/addProduct";
+import { getDocuments, getDocumentsOrder } from "@/functions/firebase/getData";
+import Loader from "@/components/common/Loader";
 
-const AddProductPage = ({subcats,cats}) => {
-    return (
-            <div>
-                <AddProductMain
-                subcats={subcats}
-                cats={cats}
-                />
-        </div>
-    );
-}
+const AddProductPage = ({}) => {
+  const [products, setProducts] = useState([]);
+  const [subcats, setsubCats] = useState([]);
+  const [subcategory, setSubCategory] = useState(null);
+  const [pageLoading, setPageLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      setPageLoading(true);
+      let productsdata = [];
+
+      productsdata = await getDocumentsOrder(
+        "products",
+        orderBy("timeStamp", "desc"),
+
+        null
+      );
+
+      const subcatsdata = await getDocumentsOrder(
+        "subcats",
+        orderBy("timeStamp", "asc"),
+
+        null
+      );
+
+      setProducts(productsdata);
+      setsubCats(subcatsdata);
+      setSubCategory(subcategorydata);
+      setPageLoading(false);
+    };
+    getData();
+  }, []);
+
+  if (pageLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <div>
+      <AddProductMain subcats={subcats} cats={cats} />
+    </div>
+  );
+};
 
 export default AddProductPage;
 
-
 // serverside
-AddProductPage.getInitialProps = async (context) => {
-    const SubCategories = await getDocuments("subcats"); //  []
-    const Categories = await getDocuments("cats"); //  []
-  
-    console.log("data SubCats💡", SubCategories);
-    console.log("data Cats💡", Categories);
-  
-    return {
-      // props from serverside will go to props in clientside
-      subcats: SubCategories,
-      cats: Categories,
-    };
-  };
-  
+// AddProductPage.getInitialProps = async (context) => {
+//   const SubCategories = await getDocuments("subcats"); //  []
+//   const Categories = await getDocuments("cats"); //  []
+
+//   console.log("data SubCats💡", SubCategories);
+//   console.log("data Cats💡", Categories);
+
+//   return {
+//     // props from serverside will go to props in clientside
+//     subcats: SubCategories,
+//     cats: Categories,
+//   };
+// };
